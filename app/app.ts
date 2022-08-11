cf7c29231812480631d07ca62c0361bfe7ae2fbc
danilojpfreitas <danilojpfreitas@gmail.com>
2022-08-10 21:36:31 -0300

    Parte 2

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,15 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
 
 const controller = new NegociacaoController;
-const form = document.querySelector(".form")
-form.addEventListener("submit", (event: Event) => {
+const form = document.querySelector(".form");
+if (form) {
+    form.addEventListener("submit", (event: Event) => {
     event.preventDefault();
     controller.adiciona();
-});
+    });
+} else {
+    throw Error("Não foi possível inicializar a aplicação. Verifique se o form existe.")
+}
+
 
 
